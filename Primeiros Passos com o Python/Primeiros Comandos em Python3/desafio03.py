5cf920f0504b02060db48fcf9054d90572a921a4
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-20 16:01:48 -0300

    Resolucao do Terceiro Desafio de Python

---
+++ Primeiros Passos com o Python/Primeiros Comandos em Python3/desafio03.py
@@ -0,0 +1,5 @@
+print("==================== DESAFIO 03 ====================")
+firstNumber = int(input("Primeiro número: "))
+secondNumber = int(input("Segundo número: "))
+result = firstNumber + secondNumber
+print("A soma é igual a",result)
