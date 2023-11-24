ff4fe454e1135f5a9927cc3e6e767f865e33a03f
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-24 11:03:16 -0300

    Resolucao do Desafio 008

---
+++ Tratando Dados e Fazendo Contas/Operadores Aritmeticos/Desafio_008/main.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
+
+number = float(input('Digite um valor em metros para calcular a conversão para centímetros e milímetros: '))
+
+print(f'{number} m é igual a {number * 100} cm ou {number * 1000} mm')
