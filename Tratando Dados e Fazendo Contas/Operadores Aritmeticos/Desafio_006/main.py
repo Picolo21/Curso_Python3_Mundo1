183f26ba7429e8741c1d8227741eb529efa682f1
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-24 10:49:17 -0300

    Resolucao do Desafio 006

---
+++ Tratando Dados e Fazendo Contas/Operadores Aritmeticos/Desafio_006/main.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+number = float(input('Digite um número qualquer: '))
+
+print(f'O dobro de {number} é igual a {number * 2}')
+print(f'O triplo de {number} é igual a {number * 3}')
+print(f'A raiz quadrada de {number} é igual a {number ** (1 / 2)}')
