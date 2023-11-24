17fd9bab72812d6825c0b83bca321010ecc29101
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-24 10:41:12 -0300

    Resolucao do Desafio 005

---
+++ Tratando Dados e Fazendo Contas/Operadores Aritmeticos/Desafio_005/main.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor
+
+number = int(input('Digite um número inteiro qualquer: '))
+print(f'O sucessor de {number} é {number + 1}')
+print(f'O antecessor de {number} é {number - 1}')
