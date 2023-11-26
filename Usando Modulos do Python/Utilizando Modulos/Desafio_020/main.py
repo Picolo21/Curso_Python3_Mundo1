aafc51175b412d588e9a75708eef7f801f28a4a4
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-26 16:17:33 -0300

    Resolucao do Desafio 020

---
+++ Usando Modulos do Python/Utilizando Modulos/Desafio_020/main.py
@@ -0,0 +1,15 @@
+# Um professor quer sortear a ordem de apresentação dos trabalhos dos alunos. Faça um programa
+# que leia o nome dos quatro alunos e mostre a ordem sorteada
+
+import random
+
+person1 = input('Primeiro(a) aluno(a): ')
+person2 = input('Segundo(a) aluno(a): ')
+person3 = input('Terceiro(a) aluno(a): ')
+person4 = input('Quarto(a) aluno(a): ')
+
+personList = [person1, person2, person3, person4]
+random.shuffle(personList)
+
+print('A ordem de apresentação dos alunos será:')
+print(personList)
