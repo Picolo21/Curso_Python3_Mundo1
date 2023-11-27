c91050a5438c48da9ce03588463bc53441834139
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-27 16:23:26 -0300

    Resolucao do Desafio 031

---
+++ Condicoes em Python/Condicoes (Parte 1)/Desafio_031/main.py
@@ -0,0 +1,18 @@
+# Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da
+# passagem, cobrando R$ 0,50 por Km para viagens de até 200 Km e R$ 0,45 para viagens
+# mais longas
+
+parameterDistance = 200
+
+distance = float(input('Informe a distância da sua viagem (em Km): '))
+
+if distance < 0:
+    print('Você informou uma distância que não é válida')
+elif distance <= parameterDistance:
+    value = distance * 0.50
+    print(f'O valor da sua passagem será de R$ {value:.2f}')
+else:
+    value = distance * 0.45
+    print(f'O valor da sua passagem será de R$ {value:.2f}')
+
+
