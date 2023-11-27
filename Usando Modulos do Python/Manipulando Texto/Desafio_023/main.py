581ad9d94d17bfaa97762d6f002238459165e683
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-27 01:38:25 -0300

    Correcao do Desafio 023

--- Usando Modulos do Python/Manipulando Texto/Desafio_023/main.py
+++ Usando Modulos do Python/Manipulando Texto/Desafio_023/main.py
@@ -1,4 +1,4 @@
-# Faça um programa que leia um número de 1000 a 9999 e mostre na tela cada um dos
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos
 # dígitos separados
 
 # Exemplo: Digite um número: 1834
@@ -8,9 +8,14 @@
 # Centena: 8
 # Milhar: 1
 
-value = input('Digite um valor qualquer entre 1000 e 9999: ')
+value = int(input('Digite um valor qualquer entre 0 e 9999: '))
 
-print(f'Unidade: {value[3]}')
-print(f'Dezena: {value[2]}')
-print(f'Centena: {value[1]}')
-print(f'Milhar: {value[0]}')
+u = value // 1 % 10
+d = value // 10 % 10
+c = value // 100 % 10
+m = value // 1000 % 10
+
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
